e04cd21056344fb84b35a27a45048617498e433d
xCrypt0r <legacy5877@gmail.com>
2025-04-24 08:16:20 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,28 +1,27 @@
 self.onmessage = function (e) {
     if (e.data.type === 'csv') {
-        let data = e.data.content;
+        let data = e.data.content.split(/\r?\n/)
+            .slice(1)
+            .map((record) => record.split(','))
+            .filter((record) => record[5] === 'O')
+            .map((record) => ({
+                word: record[0],
+                species: record[1].split('|'),
+                parts: record[2].split('|'),
+                categories: record[3].split('|'),
+                topics: record[4].split('|'),
+                firstCharacter: record[0].at(0),
+                lastCharacter: record[0].at(-1),
+                firstCharacterPhonemes: record[6],
+                allPhonemes: record[7],
+                lastCharacterPhonemes: record[8],
+            }));
 
         self.postMessage({
             type: 'db',
-            db: data
-                .split(/\r?\n/)
-                .slice(1)
-                .map((record) => record.split(','))
-                .filter((record) => record[5] === 'O')
-                .map((record) => ({
-                    word: record[0],
-                    species: record[1].split('|'),
-                    parts: record[2].split('|'),
-                    categories: record[3].split('|'),
-                    topics: record[4].split('|'),
-                    firstCharacter: record[0].at(0),
-                    lastCharacter: record[0].at(-1),
-                    firstCharacterPhonemes: record[6],
-                    allPhonemes: record[7],
-                    lastCharacterPhonemes: record[8],
-                })),
-            firstCharacterfrequency: getFrequency(DB, true),
-            lastCharacterfrequency: getFrequency(DB, false)
+            db: data,
+            firstCharacterfrequency: getFrequency(data, true),
+            lastCharacterfrequency: getFrequency(data, false)
         });
     }
 };
